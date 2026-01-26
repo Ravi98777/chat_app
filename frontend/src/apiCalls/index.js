85c5009b5ff0d995353d99e4979897e68e0953bf
Ravi Sharma <hisharmaji96@gmail.com>
2026-01-27 02:23:55 +0530

    Update API URL to production endpoint

--- frontend/src/apiCalls/index.js
+++ frontend/src/apiCalls/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const url = "http://localhost:5000";
+export const url = "https://backend-chat-app-xv55.onrender.com";
 
 export const axiosInstance = axios.create({
   baseURL: url,
